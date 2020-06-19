53bc792fd321b45f71a31320fcd67f335d474781
Domi04151309 <domi04151309business@gmail.com>
2020-06-19 17:08:08 +0200

    Try new postprocess script

--- js/postprocess.js
+++ js/postprocess.js
@@ -1,29 +1,29 @@
-document.addEventListener("DOMContentLoaded", function(){
-  document.querySelector("h2, h3").forEach((item, i) => {
-    var headingText = item.innerHTML.toLowerCase().trim().replace(/[\.,-\/#!?$%\^&\*;:{}=\-_`~()]/g,"").replace(/[ ]/g,"-");
-    item.id = headingText;
-    item.prepend( "<a href='#" + headingText + "' class='hash'>&#35;</a>" );
-  });
-  document.querySelector("img").forEach((item, i) => {
-    var wrapper = document.createElement("a");
-    wrapper.href = item.src;
-    item.parentNode.insertBefore(wrapper, item);
-    wrapper.appendChild(item);
-  });
-  document.querySelector(".hash").forEach((item, i) => {
-    item.preventDefault();
+document.querySelectorAll("h2, h3").forEach((item) => {
+  const headingText = item.innerHTML.toLowerCase().trim().replace(/[\.,-\/#!?$%\^&\*;:{}=\-_`~()]/g,"").replace(/[ ]/g,"-");
+  item.id = headingText;
+  item.innerHTML = "<a href='#" + headingText + "' class='hash'>&#35;</a>" + item.innerHTML;
+});
+
+document.querySelectorAll("img").forEach((item) => {
+  const wrapper = document.createElement("a");
+  wrapper.href = item.src;
+  item.parentNode.insertBefore(wrapper, item);
+  wrapper.appendChild(item);
+});
+
+document.querySelectorAll(".hash").forEach((item) => {
+  item.addEventListener("click", function(e){
+    e.preventDefault();
     $('html,body').animate({
-      scrollTop: $(item.href).offset().top - 86
+      scrollTop: item.offsetTop - 86
     }, 500);
     window.history.pushState(null, null, item.href);
   });
-  setTimeout(loadHash, 1000);
 });
-
-function loadHash(){
+setTimeout(function(){
   if (location.hash) {
     $('html,body').animate({
-      scrollTop: $(location.hash).offset().top - 86
+      scrollTop: document.getElementById(location.hash.substring(1)).offsetTop - 86
     }, 500);
   }
-}
+}, 1000);
