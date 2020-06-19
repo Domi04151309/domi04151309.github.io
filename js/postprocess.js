a0273872a7886016b80536da2b7c6899164e68ce
Domi04151309 <domi04151309business@gmail.com>
2020-06-19 16:31:17 +0200

    Try new postprocess script

--- js/postprocess.js
+++ js/postprocess.js
@@ -1,26 +1,29 @@
-$(document).ready(function(){
-  $("h2, h3").each(function(i){
-    var heading = $(this);
-    var headingtext = heading.text().toLowerCase().trim().replace(/[\.,-\/#!?$%\^&\*;:{}=\-_`~()]/g,"").replace(/[ ]/g,"-");
-    heading.attr("id",headingtext);
-    heading.prepend( "<a href='#" + headingtext + "' class='hash'>&#35;</a>" );
+document.addEventListener("DOMContentLoaded", function(){
+  document.querySelector("h2, h3").forEach((item, i) => {
+    var headingText = item.innerHTML.toLowerCase().trim().replace(/[\.,-\/#!?$%\^&\*;:{}=\-_`~()]/g,"").replace(/[ ]/g,"-");
+    item.id = headingText;
+    item.prepend( "<a href='#" + headingText + "' class='hash'>&#35;</a>" );
   });
-  $("img").each(function(i){
-    var image = $(this);
-    var imageSrc = $(this).attr('src');
-    image.wrap( "<a href='" + imageSrc + "'></a>" );
+  document.querySelector("img").forEach((item, i) => {
+    var wrapper = document.createElement("a");
+    wrapper.href = item.src;
+    item.parentNode.insertBefore(wrapper, item);
+    wrapper.appendChild(item);
   });
-  $(".hash").click(function(e){
-    e.preventDefault();
-    var aid = $(this).attr("href");
-    $('html,body').animate({scrollTop: $(aid).offset().top-86},500);
-    window.history.pushState(null, null, aid);
+  document.querySelector(".hash").forEach((item, i) => {
+    item.preventDefault();
+    $('html,body').animate({
+      scrollTop: $(item.href).offset().top - 86
+    }, 500);
+    window.history.pushState(null, null, item.href);
   });
-  setTimeout(loadHash,1000);
+  setTimeout(loadHash, 1000);
 });
+
 function loadHash(){
-  if(location.hash) {
-    var aid = location.hash;
-    $('html,body').animate({scrollTop: $(aid).offset().top-86},500);
+  if (location.hash) {
+    $('html,body').animate({
+      scrollTop: $(location.hash).offset().top - 86
+    }, 500);
   }
 }
