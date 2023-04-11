827e85d560615cdad933b5ced4e0f6323bb6ce46
domi04151309 <dominikworldlp@gmail.com>
2023-04-11 20:21:51 +0200

    Redesign home page

--- js/postprocess.js
+++ js/postprocess.js
@@ -1,33 +1,12 @@
-document.querySelectorAll('h2, h3').forEach(item => {
-  const headingText = item.innerHTML.toLowerCase().trim().replace(/[\.,-\/#!?$%\^&\*;:{}=\-_`~()]/g,'').replace(/[ ]/g,'-');
-  item.id = headingText;
-  item.innerHTML = '<a href="#' + headingText + '" class="hash">&#35;</a>' + item.innerHTML;
-});
-
-document.querySelectorAll('img').forEach(item => {
-  const wrapper = document.createElement('a');
-  wrapper.href = item.src;
-  item.parentNode.insertBefore(wrapper, item);
-  wrapper.appendChild(item);
-});
-
-document.querySelectorAll('.hash').forEach(item => {
-  item.addEventListener('click', (e) => {
-    e.preventDefault();
-    scrollToY(item.offsetTop - 86, 500, 'easeInOutQuint');
-    window.history.pushState(null, null, item.href);
-  });
-});
-
-const headers = document.getElementsByTagName('HEADER')
-const topBtn = document.getElementById('to-top')
-let i = 0
+const headers = document.getElementsByTagName('HEADER');
+const topBtn = document.getElementById('to-top');
+let i = 0;
 
 function checkClasses() {
   for (i = 0; i < headers.length; i++) {
-    headers[i].classList.toggle('header-shadow', window.pageYOffset > 0)
+    headers[i].classList.toggle('header-shadow', window.pageYOffset > 0);
   }
-  topBtn.classList.toggle('hidden', window.pageYOffset == 0)
+  topBtn.classList.toggle('hidden', window.pageYOffset == 0);
 }
-checkClasses()
-document.addEventListener('scroll', checkClasses)
+checkClasses();
+document.addEventListener('scroll', checkClasses);
